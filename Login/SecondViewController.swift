285e578d75e8fb11ec8e8c6ee380b7e256505141
efrosinina <e.efrosininaa@gmail.com>
2023-04-08 19:16:27 +0300

    * homework initial commit, add shadow, IBOutlets and IBActions

---
+++ Login/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  Login
+//
+//  Created by Елизавета Ефросинина on 07/04/2023.
+//
+
+import Foundation
+
+final class SecondViewController: ViewController {
+    
+}
---
+++ Login/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Елизавета Ефросинина on 07/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: -- IBOutlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailLineView: UIView!
+    @IBOutlet weak var passwordLineView: UIView!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    //MARK: -- IBActions
+    @IBAction func loginButtonAction(_ sender: UIButton) {
+        
+    }
+    
+    //MARK: -- Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addShadow()
+    }
+    
+    //MARK: -- Methods
+    private func addShadow() {
+        loginButton.layer.shadowColor = UIColor.black.cgColor
+        loginButton.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        loginButton.layer.shadowOpacity = 0.2
+        loginButton.layer.shadowRadius = 2
+    }
+}
+
+
