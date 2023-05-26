45660d85a071dc31483e2e2110009bb3c4beb13c
efrosinina <e.efrosininaa@gmail.com>
2023-05-26 16:24:54 +0300

    commit with adding keychain

---
+++ Login/KeyChainManager.swift
@@ -0,0 +1,49 @@
+//
+//  KeyChainManager.swift
+//  Login
+//
+//  Created by Елизавета Ефросинина on 26/05/2023.
+//
+
+import Foundation
+import Security
+
+final class KeyChainManager {
+    private static let service = "Login"
+    static func save(email: String, password: String) -> Bool {
+        let passwordData = password.data(using: .utf8)
+        
+        let query: [NSString: AnyObject] = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: email as AnyObject,
+            kSecAttrService: service as AnyObject,
+            kSecValueData: passwordData as AnyObject
+        ]
+        
+        let status = SecItemAdd(query as CFDictionary, nil)
+        
+        return status == errSecSuccess
+    }
+    
+    static func checkUser(with email: String, password: String) -> Bool {
+        let query: [NSString: Any] = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: email,
+            kSecAttrService: service,
+            kSecReturnData: true
+        ]
+        
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+        
+        if status == errSecSuccess,
+           let data = result as? Data {
+            let savedPassword = String(data: data, encoding: .utf8)
+            
+            return password == savedPassword
+        } else {
+            debugPrint("Error for getting password from Keychain")
+            return false
+        }
+    }
+}
--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -17,8 +17,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var letterImageView: UIImageView!
     @IBOutlet weak var lockImageView: UIImageView!
+    @IBOutlet weak var donthaveAnAccountLabel: UILabel!
     
     //MARK: -- Properties
+    var isLoginScreen: Bool = true
     private let wrongColor = UIColor.red
     private let defaultColor = UIColor.systemGray4
     private var email = "" {
@@ -33,21 +35,22 @@ class ViewController: UIViewController {
             loginButton.alpha = !(email.isEmpty && password.isEmpty) ? 1 : 0.5
         }
     }
-    private let mockEmail = "abc@gmail.com"
-    private let mockPassword = "123Az*"
+    
     private let emailRegex = #"^\S+@\S+\.\S+$"#
-    private let passwordRegex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[$@$!%*?&#])[A-Za-z\\d$@$!%*?&#]{4,}$"
     
     //MARK: -- Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        donthaveAnAccountLabel.isHidden = !isLoginScreen
+        signUpButton.isHidden = !isLoginScreen
         emailTextField.delegate = self
         passwordTextField.delegate = self
         addShadow()
         emailTextField.becomeFirstResponder()
         loginButton.isUserInteractionEnabled = false
         loginButton.alpha = 0.5
+        setupLoginButton()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -60,32 +63,48 @@ class ViewController: UIViewController {
         emailTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
         
-        if check(email: emailTextField.text ?? "") && check(password: passwordTextField.text ?? "") {
-            performSegue(withIdentifier: "goToNextScreen", sender: sender)
-            lockImageView.tintColor = defaultColor
-            passwordLineView.backgroundColor = defaultColor
-            letterImageView.tintColor = defaultColor
-            emailLineView.backgroundColor = defaultColor
+        if isLoginScreen {
+            if KeyChainManager.checkUser(with: email, password: password) {
+                performSegue(withIdentifier: "goToNextScreen", sender: sender)
+                lockImageView.tintColor = defaultColor
+                passwordLineView.backgroundColor = defaultColor
+                letterImageView.tintColor = defaultColor
+                emailLineView.backgroundColor = defaultColor
+            } else {
+                makeErrorField(textField: emailTextField)
+                makeErrorField(textField: passwordTextField)
+                let alert = UIAlertController(title: "Error".localized,
+                                              message: "Wrong e-mail or password".localized,
+                                              preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK".localized, style: .default)
+                alert.addAction(action)
+                present(alert, animated: true)
+            }
         } else {
-            makeErrorField(textField: emailTextField)
-            makeErrorField(textField: passwordTextField)
-            let alert = UIAlertController(title: "Error".localized,
-                                          message: "Wrong e-mail or password".localized,
-                                          preferredStyle: .alert)
-            let action = UIAlertAction(title: "OK".localized, style: .default)
-            alert.addAction(action)
-            present(alert, animated: true)
+            if KeyChainManager.save(email: email, password: password) {
+                performSegue(withIdentifier: "goToNextScreen", sender: sender)
+            } else {
+                debugPrint("Error with saving email and password")
+            }
         }
-        
     }
     
-    @IBAction func signUpButtonAction(_ sender: UIButton) {}
-    //MARK: -- Methods
+    @IBAction func signUpButtonAction(_ sender: UIButton) {
+        guard let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as? ViewController else { return }
+        viewController.isLoginScreen = false
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    //MARK: -- Private Methods
     private func addShadow() {
         loginButton.layer.shadowColor = UIColor.black.cgColor
         loginButton.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
         loginButton.layer.shadowOpacity = 0.2
         loginButton.layer.shadowRadius = 2
+    }
+    
+    private func setupLoginButton() {
+        loginButton.setTitle(isLoginScreen ? "Login".localized.uppercased() : "Register".localized.uppercased(), for: .normal)
     }
 }
 
@@ -122,13 +141,14 @@ extension ViewController: UITextFieldDelegate {
             print("Error")
         }
     }
+    
     //MARK: -- Methods for Extension
     private func check(email: String) -> Bool {
-        return email == mockEmail && email.match(emailRegex) && !email.isEmpty
+        return email.match(emailRegex) && !email.isEmpty
     }
     
     private func check(password: String) -> Bool {
-        return password == mockPassword && password.count >= 4 && password.match(passwordRegex)
+        return password.count >= 4
     }
     
     private func makeErrorField(textField: UITextField) {
